afe26790f720973750f55abe4e5802ab1f417ebe
Stanislav Gusev <247@onlinepbx.ru>
2018-08-17 14:46:12 +0500

    added playback animation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 var numberOfTracks = 6;
 var isPlaying = false;
+var playButton = document.querySelector('.play');
+var playing = document.querySelectorAll('.playing')
 
 function getRandomNumber(max) {
   return Math.floor(Math.random() * (max - 1)) + 1;
 }
 
+function togglePlaybackAnimation() {
+  playButton.classList.toggle('display-none')
+  playing.forEach(element => {
+    console.log('foreaching')
+    element.classList.toggle('display-none')
+  })
+}
+
 function playAudio() {
   if (!isPlaying) {
+    togglePlaybackAnimation()
     var trackNumber = getRandomNumber(numberOfTracks);
     var audio = new Audio(`./audio/0${trackNumber}.wav`);
-    audio.crossOrigin = "anonymous";
     audio.play();
     isPlaying = true;
   }
   audio.onended = () => {
+    togglePlaybackAnimation();
     isPlaying = false;
   }
 }
