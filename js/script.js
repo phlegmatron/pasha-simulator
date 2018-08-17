0872b71c2b0158f37ae6898dc9b7fe5b786889b8
Stanislav Gusev <247@onlinepbx.ru>
2018-08-17 13:55:09 +0500

    testing web audio api

--- js/script.js
+++ js/script.js
@@ -9,10 +9,69 @@ function playAudio() {
   if (!isPlaying) {
     var trackNumber = getRandomNumber(numberOfTracks);
     var audio = new Audio(`./audio/0${trackNumber}.wav`);
+    audio.crossOrigin = "anonymous";
+    analyze(audio);
     audio.play();
     isPlaying = true;
   }
   audio.onended = () => {
     isPlaying = false;
   }
+}
+
+function analyze(audio) {
+  var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+  var analyser = audioCtx.createAnalyser();
+  var source = audioCtx.createMediaElementSource(audio);
+  source.connect(audioCtx.destination);
+
+  // ...
+
+  analyser.fftSize = 2048;
+  var bufferLength = analyser.frequencyBinCount;
+  var dataArray = new Uint8Array(bufferLength);
+  analyser.getByteTimeDomainData(dataArray);
+
+  // Get a canvas defined with ID "oscilloscope"
+  var canvas = document.getElementById("oscilloscope");
+  var canvasCtx = canvas.getContext("2d");
+
+  // draw an oscilloscope of the current audio source
+
+  function draw() {
+
+    requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = "rgb(200, 200, 200)";
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+    canvasCtx.beginPath();
+
+    var sliceWidth = canvas.width * 1.0 / bufferLength;
+    var x = 0;
+
+    for (var i = 0; i < bufferLength; i++) {
+
+      var v = dataArray[i] / 128.0;
+      var y = v * canvas.height / 2;
+
+      if (i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.stroke();
+  }
+
+  draw();
 }
